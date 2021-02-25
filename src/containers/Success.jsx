6ff25fef0100025f95c2ebbe0db024b428509eeb
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2021-02-25 05:18:47 -0300

    Se integra GoogleMaps en componente Success

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import Map from '../components/Map';
 
-import '../styles/components/Success.css'
+import '../styles/components/Success.css';
 
 const Success = () => {
+  const { state } = useContext(AppContext);
+  const { buyer } = state;
+  
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>GaBo, Gracias por tu Compra</h2>
+        <h2>{`${buyer.name}, Gracias por tu Compra`}</h2>
         <span>Tu pedido llegará en 3 dia a tu dirección</span>
         <div className="Success-map">
-          GoogleMaps
+          <Map/>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Success;
