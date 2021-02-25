d61a663a0c3886905af0062e332dea2631d5dfc5
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2021-02-25 08:52:16 -0300

    [add] react-helmet

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import Products from '../components/Products'
+import { Helmet } from 'react-helmet';
+import Products from '../components/Products';
 import initialState from '../initialState';
 
 const Home = () => {
   return (
-    <Products products={initialState.products} />
+    <>
+      <Helmet>
+        <title>Platzi Conf Merch - Productos</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@funk_gabo" />
+        <meta name="twitter:creator" content="@funk_gabo" />
+        <meta name="twitter:title" content="Platzi Conf Store" />
+        <meta name="twitter:description" content="Platzi Conf Store" />
+        <meta
+          name="twitter:image"
+          content=""
+        />
+        <meta property="og:title" content="Platzi Conf Store" />
+        <meta property="og:description" content="Platzi Conf Store" />
+        <meta
+          property="og:image"
+          content=""
+        />
+        <meta property="og:url" content="gabofortesting.xyz" />
+        <meta property="og:site_name" content="Platzi Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="" />
+      </Helmet>
+      <Products products={initialState.products} />;
+    </>
   );
-}
+};
 
 export default Home;
